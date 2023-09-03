7c4d453a64822f6397e38119e7ddd9a357fc5d09
Sandra Katsikis <mindfulripples@gmail.com>
2023-09-03 20:58:20 +1000

    lab capitalisation complete

--- index.js
+++ index.js
@@ -10,7 +10,26 @@ const tutorials = [
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
 ];
+ 
+function titleCased(list) {
+  const listUp = list.map(function(title){
+      //split title (the individual string) into an Array of strings (wordArray) whenever a blank space is encountered
+      const wordArray = title.split(" ");
 
-const titleCased = () => {
-  return tutorials
-}
+      //loop through each element of the array and capitalise the first letter using.map
+      const capitalWordArray = wordArray.map(function(word){
+          return (word.charAt(0).toUpperCase() + word.slice(1))
+          
+      })
+
+      //join the elements back into a string
+      let capArray = capitalWordArray.reduce((accumulator, word) => accumulator + " " + word);
+      
+      console.log(typeof(capArray));
+             
+      })
+        
+    }
+
+  titleCased(tutorials);
+
